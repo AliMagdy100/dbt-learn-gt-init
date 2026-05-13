f7e22306b95f47dc04a2b6d898e042b3898f7eb8
Ali Magdy <alimagdy100@gmail.com>
2026-05-13 19:29:37 +0300

    add yaml config for sources

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -2,6 +2,7 @@ select
     id as order_id,
     user_id as customer_id,
     order_date,
-    status
+    status,
+    _loaded_at
 
-from raw.jaffle_shop.orders
+from {{source('jaffle_shop', 'orders')}}
